68302e9ccfbd1adf8ae5ad35b3f931052d32ecd7
Alison Williams <alison.lauriewill@gmail.com>
2019-07-01 16:54:05 +0100

    MVP completed

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
 
-
 class Film extends Component {
     render(){
         return (
-            <p>Hello!</p>
+            <li className="film"><a href={this.props.url}>"{this.props.name}"</a></li>
+            
         )
     }
 
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -4,9 +4,22 @@ import Film from './Film.js'
 
 class FilmList extends Component {
     render() {
+
+        const filmComponents = this.props.films.map(filmData => {
+            return (
+                <Film 
+                id={filmData.id}
+                name={filmData.name}
+                url={filmData.url}/>
+            )
+        })
+
+
         return (
             <Fragment>
-                <Film />
+                <ul>
+                    {filmComponents}
+                </ul>
             </Fragment>
         )
     }
